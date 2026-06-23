e087a609db4a68a2b404b79a49221f0c12875cbf
myne74 <54963259+myne74@users.noreply.github.com>
2026-06-23 00:38:45 -0700

    Initial setup
    Defining the basic structure for the project with 2 micro services and the docs

---
+++ engagement-service/src/test/java/com/ravi/mcl/engagement/EngagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.mcl.engagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
